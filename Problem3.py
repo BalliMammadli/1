f12cda1bbb8856e849ee67892197250a7195ff29
BalliMammadli <116656794+BalliMammadli@users.noreply.github.com>
2022-10-31 08:35:14 +0400

    Add files via upload
    Salam müəllim. Eyni Listə yığmaqla bağlı problem yaşadım. Düşünürdüm ki, son aldığım list ilə nəticəni toplayıb axırda print ilə çıxışa verim amma bilmədim necə edim.

---
+++ Problem3.py
@@ -0,0 +1,5 @@
+countries = ['USA', 'Canada', 'Mexico', 'Germany', 'France', 'Spain', 'Italy', 'Greece', 'Portugal', 'UK', 'Ireland', 'Netherlands', 'Belgium', 'Luxembourg', 'Switzerland', 'Austria', 'Poland', 'Czech Republic', 'Slovakia', 'Hungary', 'Romania', 'Bulgaria', 'Serbia', 'Macedonia', 'Albania', 'Greece', 'Turkey', 'Cyprus', 'Russia', 'Ukraine', 'Belarus', 'Moldova', 'Kazakhstan', 'China', 'India', 'Japan', 'South Korea', 'Australia', 'New Zealand', 'South Africa', 'Egypt', 'Morocco', 'Tunisia', 'Algeria', 'Libya', 'Saudi Arabia', 'Iran', 'Iraq', 'Syria']
+for i in countries:
+    if 'a' in i:
+        b=i.split()
+        print(b) #eyni liste yiga bilmedim
---
+++ Problem7.py
@@ -0,0 +1,7 @@
+countries = ['USA', 'Canada', 'Mexico', 'Germany', 'France', 'Spain', 'Italy', 'Greece', 'Portugal', 'UK', 'Ireland', 'Netherlands', 'Belgium', 'Luxembourg', 'Switzerland', 'Austria', 'Poland', 'Czech Republic', 'Slovakia', 'Hungary', 'Romania', 'Bulgaria', 'Serbia', 'Macedonia', 'Albania', 'Greece', 'Turkey', 'Cyprus', 'Russia', 'Ukraine', 'Belarus', 'Moldova', 'Kazakhstan', 'China', 'India', 'Japan', 'South Korea', 'Australia', 'New Zealand', 'South Africa', 'Egypt', 'Morocco', 'Tunisia', 'Algeria', 'Libya', 'Saudi Arabia', 'Iran', 'Iraq', 'Syria']
+
+for i in countries:
+    lst=list(i)
+    if lst[0]!='P':
+        b=i.split()
+        print(b)
